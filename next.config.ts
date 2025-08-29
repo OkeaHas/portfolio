1ff3cbd7ce10e9b38c10a0b34b637f4c084eb611
RenggaX <gamingtube1985@gmail.com>
2025-08-29 18:04:15 +0800

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // biar bisa di-export jadi static file (HTML, CSS, JS)
+  output: "export",
+
+  // basePath & assetPrefix = nama repo kamu
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
 };
 
 export default nextConfig;
